81186e8194d3dda666d5df6234d10f2072c29906
MohammedSalameh <Mohammed@salameh.dk>
2021-07-29 22:39:16 +0200

    Getting data from server

--- 12.2-starting-project(Multi-page-practice)/src/pages/NewQuote.js
+++ 12.2-starting-project(Multi-page-practice)/src/pages/NewQuote.js
@@ -1,19 +1,30 @@
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
+import React, { useEffect } from 'react'
+
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
-  const history = useHistory();
+	const history = useHistory();
+	const { sendRequest, status } = useHttp(addQuote);
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-    // navigate away to swithc pages
-    // push allows the user to go back
-    // replace does not allow the user to go back
-    history.push('/quotes');
-  }
+  useEffect(() => {
+    if(status === 'completed') {
+      history.push('/quotes')
+    }
 
+  }, [status, history])
 
-	return <QuoteForm onAddQuote={addQuoteHandler}/>;
+	const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+		// navigate away to swithc pages
+		// push allows the user to go back
+		// replace does not allow the user to go back
+		
+	};
+
+	return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
--- 12.2-starting-project(Multi-page-practice)/src/pages/QuoteDetail.js
+++ 12.2-starting-project(Multi-page-practice)/src/pages/QuoteDetail.js
@@ -1,26 +1,48 @@
 import { Route, useParams, Link, useRouteMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import useHttp from '../hooks/use-http';
 
-const DUMMY_QUOTES = [
-	{ id: '1', author: 'Mummi', text: 'learning react is fun' },
-	{ id: '2', author: 'Snow', text: 'imagine learning angular yikes' },
-];
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
 const QuoteDetail = () => {
 	const match = useRouteMatch();
 	const params = useParams();
+	const { quoteId } = params;
 
-	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+	const {
+		sendRequest,
+		status,
+		data: loadedQuote,
+		error,
+	} = useHttp(getSingleQuote, true);
 
-	if (!quote) {
-		return <p>No Quote Found!</p>;
+	useEffect(() => {
+		sendRequest(quoteId);
+	}, [sendRequest, quoteId]);
+
+	if (status === 'pending') {
+		return (
+			<div className='centered'>
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p className='centered'>{error}</p>;
+	}
+
+	if (!loadedQuote.text) {
+		return <p className='centered'>No Quote Found</p>;
 	}
 
 	return (
 		<>
-			<HighlightedQuote text={quote.text} author={quote.author} />
+			<HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 			<Route path={match.path} exact>
 				<div className='centered'>
 					<Link className='btn--flat' to={`${match.url}/comments`}>
