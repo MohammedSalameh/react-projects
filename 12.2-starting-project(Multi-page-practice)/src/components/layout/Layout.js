8e84552ff3dfc71408a43766a24cd5a21b0a2c35
MohammedSalameh <Mohammed@salameh.dk>
2021-07-29 21:38:01 +0200

    Dummy data

--- 12.2-starting-project(Multi-page-practice)/src/components/layout/Layout.js
+++ 12.2-starting-project(Multi-page-practice)/src/components/layout/Layout.js
@@ -6,9 +6,7 @@ const Layout = (props) => {
 	return (
 		<>
 			<MainNavigation />
-      <main className={classes.main}>
-      {props.children}
-      </main>
+			<main className={classes.main}>{props.children}</main>
 		</>
 	);
 };
--- 12.2-starting-project(Multi-page-practice)/src/pages/AllQuotes.js
+++ 12.2-starting-project(Multi-page-practice)/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+	{ id: 1, author: 'Mummi', text: 'learning react is fun' },
+	{ id: 2, author: 'Snow', text: 'imagine learning angular yikes' },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
-}
+	return <QuoteList quotes={DUMMY_QUOTES}/>
+};
 
 export default AllQuotes;
