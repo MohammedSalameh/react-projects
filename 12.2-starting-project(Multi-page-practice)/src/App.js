9730ec35daaac45b8a856d8b1bb7fc1c404dbe33
MohammedSalameh <Mohammed@salameh.dk>
2021-07-29 21:14:02 +0200

    Practice project setup

--- 12.2-starting-project(Multi-page-practice)/src/App.js
+++ 12.2-starting-project(Multi-page-practice)/src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
 
-    </div>
-  );
+
+
+function App() {
+	return (
+		<Switch>
+			<Route path='/quotes' exact>
+				<AllQuotes />
+			</Route>
+			<Route path='/new-quote' exact>
+				<NewQuote />
+			</Route>
+			<Route path='/quotes/:quoteId'>
+				<QuoteDetail />
+			</Route>
+
+
+		</Switch>
+	);
 }
 
 export default App;
