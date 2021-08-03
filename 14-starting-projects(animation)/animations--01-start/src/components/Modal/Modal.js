7b937c7cb0322f062950df1a14ec97e20754f303
MohammedSalameh <Mohammed@salameh.dk>
2021-08-03 19:13:54 +0200

    Animatino Timings

--- 14-starting-projects(animation)/animations--01-start/src/components/Modal/Modal.js
+++ 14-starting-projects(animation)/animations--01-start/src/components/Modal/Modal.js
@@ -3,15 +3,22 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+	enter: 400,
+	exit: 1000
+};
+
 const modal = (props) => {
 	return (
-		<Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
-			{state => {
-				
+		<Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+			{(state) => {
 				const cssClasses = [
 					'Modal',
-					state === 'entering' ? 'ModalOpen'
-					: state === 'exiting'	? 'ModalClosed' : null,
+					state === 'entering'
+						? 'ModalOpen'
+						: state === 'exiting'
+						? 'ModalClosed'
+						: null,
 				];
 
 				return (
