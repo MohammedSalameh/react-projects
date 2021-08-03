8a8bac1b7526efc816eefcb5a562b68872bd6f5d
MohammedSalameh <Mohammed@salameh.dk>
2021-08-03 19:16:40 +0200

    Transition Events
    Execute code according to events

--- 14-starting-projects(animation)/animations--01-start/src/App.js
+++ 14-starting-projects(animation)/animations--01-start/src/App.js
@@ -29,7 +29,15 @@ const App = () => {
 				Toggle
 			</button>
 
-			<Transition in={showBlock} timeout={1000} mountOnEnter unmountOnExit>
+			<Transition in={showBlock} timeout={1000} 
+      mountOnEnter 
+      unmountOnExit 
+      onEnter={() => console.log('onEnter')}
+      onEntering={() => console.log('onEntering')}
+      onEntered={() => console.log('onEntered')}
+      onExit={() => console.log('onExit')}
+      onExiting={() => console.log('onExiting')}
+      onExited={() => console.log('onExited')}>
 				{(state) => (
 					<div
 						style={{
