52a365955b1df1313fd0f0649131ade79ac9df2d
MohammedSalameh <Mohammed@salameh.dk>
2021-08-05 18:35:40 +0200

    Mocking function and its return data.

--- 16-starting-project(testing)/src/components/Async.test.js
+++ 16-starting-project(testing)/src/components/Async.test.js
@@ -6,6 +6,10 @@ import Async from './Async';
 
 describe('Async component', () => {
 	test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async() => [{id: 'p1', title: 'First post'}]
+    });
 		render(<Async />);
 
 		// getBy... looks at the dom instantly.
