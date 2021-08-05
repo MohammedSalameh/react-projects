8929122319117ef6e08edd4464991215eeab07c1
MohammedSalameh <Mohammed@salameh.dk>
2021-08-05 18:17:30 +0200

    Testing user interaction & state

--- 16-starting-project(testing)/src/components/Greeting.js
+++ 16-starting-project(testing)/src/components/Greeting.js
@@ -1,8 +1,19 @@
+import React, { useState } from 'react'
+
+
 const Greeting = () => {
+  const [changedText, setChangeText] = useState(false);
+
+
+  const changeTextHandler = () => {
+    setChangeText(true)
+  }
 	return (
 		<div>
 			<h2>Hello world</h2>
-			<p>It's good to see you</p>
+			{!changedText && <p>It's good to see you</p>}
+			{changedText && <p>Changed</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
 		</div>
 	);
 };
--- 16-starting-project(testing)/src/components/Greeting.test.js
+++ 16-starting-project(testing)/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render,screen } from '@testing-library/react';
 import Greeting from './Greeting';
+import userEvent from '@testing-library/user-event'
 
 describe('Greeting Component', () => {
   test('renders Hello World as a text', () => {
@@ -12,6 +13,52 @@ describe('Greeting Component', () => {
     const helloworld = screen.getByText(/Hello World/i)
     // screen.getByText('Hello World', {exact: false})
     expect(helloworld).toBeInTheDocument();
+    
+  });
+
+  test('renders Text: "Its good to see you" if the button was NOT clicked', () => {
+    // Arrange: Test dtata and test conditions
+    render(<Greeting/>);
+  
+    // Act: Logic that should be tested
+    
+    
+    // Assert: compare exectution results
+    const text = screen.getByText(/It's good to see you/i)
+    
+    // screen.getByText('Hello World', {exact: false})
+    expect(text).toBeInTheDocument();
+    
+  });
+
+  test('render "Changed" if the button was clicked', () => {
+    // Arrange: Test dtata and test conditions
+    render(<Greeting/>);
+  
+    // Act: Logic that should be tested
+    const button = screen.getByRole('button');
+    userEvent.click(button)
+
+    // Assert: compare exectution results
+    const text = screen.getByText('Changed')
+    // screen.getByText('Hello World', {exact: false})
+    expect(text).toBeInTheDocument();
+    
+  });
+
+  test('does not render, Its good to see you, if the button was clicked', () => {
+    // Arrange: Test dtata and test conditions
+    render(<Greeting/>);
+  
+    // Act: Logic that should be tested
+    const button = screen.getByRole('button');
+    userEvent.click(button)
+
+    // Assert: compare exectution results
+    const text = screen.queryByText("It's good to see you")
+
+    // screen.getByText('Hello World', {exact: false})
+    expect(text).toBeNull();
     
   });
 
